fd15a676f28084ad0eccec6aa7c6473d32e50460
crisostomosSantos <criscgweb@gmail.com>
2019-12-10 15:06:30 -0300

    Commit inicial dos arquivos do projeto.

---
+++ api_restful_springBoot/src/main/java/com/devprojts/api_restful_springBoot/api/ApiRestfulSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.devprojts.api_restful_springBoot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringBootApplication.class, args);
+	}
+
+}
---
+++ api_restful_springBoot/src/test/java/com/devprojts/api_restful_springBoot/api/ApiRestfulSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devprojts.api_restful_springBoot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
